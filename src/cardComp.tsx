bc4e75d84f78f9351430606c98ecda4e15161e80
MuniMunii <tyuimiti@gmail.com>
2025-01-20 16:28:59 +0700

    adding card comp

---
+++ src/cardComp.tsx
@@ -0,0 +1,28 @@
+import React, { useState,useEffect } from "react";
+import { NewsItem } from "./App";
+interface dataParameter {
+  data: {
+    news: NewsItem[];
+  } | null;
+  animation:boolean|null
+  dataLength:number
+}
+const NewsCard: React.FC<dataParameter> = ({ data,animation,dataLength }) => {
+  if (!data?.news) return null;
+
+  return (
+    <div className="flex w-[80%] flex-wrap justify-center gap-4">
+      {data.news.map((data, index) => (
+        <div
+          key={data.id}
+          className={`w-96 h-fit p-4 text-white ${
+            data.notAnimated? "animate-pulse bg-white h-48" : ""
+          }`}
+        >
+          {!animation? data.title : ""}
+        </div>
+      ))}
+    </div>
+  );
+};
+export default NewsCard;
